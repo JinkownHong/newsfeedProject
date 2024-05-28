7f3bd403c9d46db2e3c0f5bb0c17b55a1dcae8f2
itssu <ssujinn96@gmail.com>
2024-05-28 11:49:53 +0900

    :sparkles: 회원가입 시 비밀번호 암호화 적용

--- src/main/kotlin/com/teamsparta/exhibitionnewsfeed/domain/user/dto/SignUpRequest.kt
+++ src/main/kotlin/com/teamsparta/exhibitionnewsfeed/domain/user/dto/SignUpRequest.kt
@@ -4,6 +4,7 @@ import com.teamsparta.exhibitionnewsfeed.domain.user.model.User
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import org.hibernate.validator.constraints.Length
+import org.springframework.security.crypto.password.PasswordEncoder
 
 data class SignUpRequest(
 
@@ -19,10 +20,10 @@ data class SignUpRequest(
 
     val description: String?
 ) {
-    fun toEntity(): User {
+    fun toEntity(passwordEncoder: PasswordEncoder): User {
         return User(
             email = this.email,
-            password = this.password,
+            password = passwordEncoder.encode(this.password),
             nickname = this.nickname,
             description = this.description
         )
--- src/main/kotlin/com/teamsparta/exhibitionnewsfeed/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/exhibitionnewsfeed/domain/user/service/UserServiceImpl.kt
@@ -3,14 +3,16 @@ package com.teamsparta.exhibitionnewsfeed.domain.user.service
 import com.teamsparta.exhibitionnewsfeed.domain.user.dto.SignUpRequest
 import com.teamsparta.exhibitionnewsfeed.domain.user.dto.SignUpResponse
 import com.teamsparta.exhibitionnewsfeed.domain.user.repository.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(
-    private val userRepository: UserRepository
-): UserService {
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) : UserService {
     override fun signUp(request: SignUpRequest): SignUpResponse {
-        if(userRepository.existsByEmail(request.email)) throw IllegalStateException("User already exists")
-        return userRepository.save(request.toEntity()).toResponse()
+        if (userRepository.existsByEmail(request.email)) throw IllegalStateException("User already exists")
+        return userRepository.save(request.toEntity(passwordEncoder)).toResponse()
     }
 }
