11e07e557849c8197078190592ddf85917168d8a
itssu <ssujinn96@gmail.com>
2024-06-01 22:41:51 +0900

    :bug: update_at 자동으로 입력 안되는 오류 수정

--- src/main/kotlin/com/teamsparta/exhibitionnewsfeed/domain/newsfeed/post/model/BaseTime.kt
+++ src/main/kotlin/com/teamsparta/exhibitionnewsfeed/domain/newsfeed/post/model/BaseTime.kt
@@ -4,16 +4,18 @@ import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
 import org.hibernate.annotations.CreationTimestamp
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @MappedSuperclass
-@EntityListeners
+@EntityListeners(AuditingEntityListener::class)
 abstract class BaseTime {
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     val createdAt: LocalDateTime? = null
 
-    @CreationTimestamp
-    @Column(nullable = false)
-    val updatedAt: LocalDateTime? = null
+    @LastModifiedDate
+    @Column(nullable = true)
+    var updatedAt: LocalDateTime? = null
 }
